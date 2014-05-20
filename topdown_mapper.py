6f952f78fbd42f6d9a6a49407d5bc966346c5d58
JoyeeCheung <joyeec9h3@gmail.com>
2014-05-20 21:15:49 +0800

    new batch scheme in topdown mapper

--- topdown_mapper.py
+++ topdown_mapper.py
@@ -8,27 +8,6 @@ topic, category, product = 5, 6, 7
 
 p = 4
 
-# C = [(
-#     (country,),
-#     (country, state),
-#     (country, state, city),
-#     (country, state, city, topic),
-#     (country, state, city, topic, category),
-#     (country, state, city, topic, category, product)
-# ), (
-#     (topic,),
-#     (topic, country),
-#     (topic, country, state),
-#     (topic, category, country, state),
-#     (topic, category, product, country, state)
-# ), (
-#     (topic, category),
-#     (topic, category, country),
-#     (topic, category, product, country)
-# ), (
-#     (topic, category, product),
-# )]
-
 C = [(
     (country, state, city, topic, category, product),
     (country, state, city, topic, category),
@@ -54,11 +33,6 @@ batch_number = len(C)
 
 useful = (country, state, city, topic, category, product, uid)
 
-# b_sort = [(country, state, city, topic, category, product),
-#           (topic, country, state, category, product),
-#           (category, country, product),
-#           (product,)]
-
 
 def read_input(file):
     for line in file:
@@ -71,11 +45,11 @@ def main():
     data = read_input(sys.stdin)
     for e in data:
         for batch in range(batch_number):
-            head = "%s|%s" % (
-                str(batch), ' '.join(e[i] for i in C[batch][-1]))
+            head = "%s|%s" % (str(batch), ' '.join(e[i] for i in C[batch][-1]))
             part = bisect_left(boundaries, head) % p
             # batch_head | head_value <TAB> country, ..., product uid
             print "%d\t%s\t%s" % (part, head, ' '.join(e[i] for i in useful))
+
 
 if __name__ == "__main__":
     main()
